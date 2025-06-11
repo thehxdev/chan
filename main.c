ba32d0969c57a0209b17f0aff5bd79f04af2829a
thehxdev <hossein.khosravi.ce@gmail.com>
2025-06-11 11:24:25 +0330

    improved the example program and added more comments
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- main.c
+++ main.c
@@ -7,15 +7,14 @@
 // set this to zero to create an unbuffered channel
 #define CHAN_CAP 5
 #define CHAN_DSIZE sizeof(int)
-#define TO_PUSH 5
-#define CONSUMERS_COUNT 4
-#define PRODUCERS_COUNT CONSUMERS_COUNT
+#define THREAD_PUSH_LIMIT 5
+#define PRODUCERS_COUNT 5
+#define CONSUMERS_COUNT 3
 
 void *producer(void *arg) {
     int i;
     Chan_t *ch = *(Chan_t**)arg;
-
-    for (i = 0; i < TO_PUSH; i++)
+    for (i = 0; i < THREAD_PUSH_LIMIT; i++)
         if (chan_push(ch, &i)) {
             printf("push error\n");
             break;
@@ -24,15 +23,11 @@ void *producer(void *arg) {
 }
 
 void *consumer(void *arg) {
-    int i, v;
+    int v;
     Chan_t *ch = *(Chan_t**)arg;
-    for (i = 0; i < TO_PUSH; i++) {
-        if (chan_pop(ch, &v)) {
-            printf("pop error\n");
-            break;
-        }
-        printf("%d: %d\n", i, v);
-    }
+    // while the channel is open, fetch data
+    while (chan_pop(ch, &v) == 0)
+        printf("%d\n", v);
     return NULL;
 }
 
@@ -48,12 +43,18 @@ int main(void) {
     for (i = 0; i < PRODUCERS_COUNT; i++)
         pthread_create(&producers[i], NULL, producer, &ch);
 
+    // Create consumer threads
     for (i = 0; i < CONSUMERS_COUNT; i++)
         pthread_create(&consumers[i], NULL, consumer, &ch);
 
+    // Wait for all producer threads to terminate
     for (i = 0; i < PRODUCERS_COUNT; i++)
         pthread_join(producers[i], NULL);
 
+    // Close the channel. Closing the channel causes `chan_pop` return
+    // non-zero integer indicating that channel is closed and also empty.
+    // So consumer threads terminate.
+    chan_close(ch);
     for (i = 0; i < CONSUMERS_COUNT; i++)
         pthread_join(consumers[i], NULL);
 
